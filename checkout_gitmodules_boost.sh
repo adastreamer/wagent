edd79161f736b21986248a5bfffba4059b5ac9b3
Eugene Khashin <adastreamer@gmail.com>
2023-07-03 01:40:10 +0300

    reset version boost

--- checkout_gitmodules_boost.sh
+++ checkout_gitmodules_boost.sh
@@ -1,14 +1,14 @@
 # update submodule
 git submodule update --init
 # set tag for boost submodules
-DEFAULT_TAG="boost-1.70.0"
+DEFAULT_TAG="boost-1.79.0"
 # update
 git config -f .gitmodules --get-regexp '^submodule\..*\.url$' |
     while read -r KEY MODULE_PATH; do
         NAME="$(echo "${KEY}" | sed 's/^submodule\.\(.*\)\.url$/\1/')"
         if [[ $NAME =~ "boost" ]]; then
           pushd "${NAME}"
-          git checkout -b new_branch_boost $DEFAULT_TAG                   
+          git checkout -b b-$DEFAULT_TAG $DEFAULT_TAG
           popd
         fi
     done
